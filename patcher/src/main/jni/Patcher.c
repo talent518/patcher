e5abf53ff7d2da6e8d90535948086b8371fb133f
阿宝 <talent518@yeah.net>
2022-01-13 16:19:59 +0800

    Update Patcher.c

--- patcher/src/main/jni/Patcher.c
+++ patcher/src/main/jni/Patcher.c
@@ -6,24 +6,24 @@
 
 JNIEXPORT jint JNICALL Java_com_baize_patcher_api_utils_AppUtils_patcher(JNIEnv* env,
 		jobject othis, jstring argv1, jstring argv2, jstring argv3) {
-	char ** argv;
+	char *argv[4];
 	int loopVar,result;
 
-	__android_log_print(ANDROID_LOG_INFO, "bspatch.c", "开始打包之。。。。。。");
-	argv = (char**) malloc(4 * sizeof(char*));
-	for (loopVar = 0; loopVar < 4; loopVar++) {
-		argv[loopVar] = (char*) malloc(100 * sizeof(char));
-	}
+	__android_log_print(ANDROID_LOG_INFO, "bspatch.c", "开始打包");
+
 	argv[0] = "bspatch";
 	argv[1] = (char*)(*env)->GetStringUTFChars(env, argv1, 0);
 	argv[2] = (char*)(*env)->GetStringUTFChars(env, argv2, 0);
 	argv[3] = (char*)(*env)->GetStringUTFChars(env, argv3, 0);
 
-	result=bspatch(argv);
+	result = bspatch(argv);
 
 	//Important:释放内存，防止内存泄露
 	(*env)->ReleaseStringUTFChars(env, argv1, argv[1]);
 	(*env)->ReleaseStringUTFChars(env, argv2, argv[2]);
 	(*env)->ReleaseStringUTFChars(env, argv3, argv[3]);
+
+	__android_log_print(ANDROID_LOG_INFO, "bspatch.c", "打包完成");
+
 	return result;
 }
